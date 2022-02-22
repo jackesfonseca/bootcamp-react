7263c5decf7161db4cf53137a5329e40bfe0c816
Jackes da Fonseca <53023400+jackesfonseca@users.noreply.github.com>
2022-02-22 14:56:58 -0300

    Create map.js

---
+++ map/map.js
@@ -0,0 +1,12 @@
+/*
+Em matemática, é quando um conjunot para por uma operação e se torna outro conjunto
+Cria um novo array
+Realiza as operações em ordem
+*/
+
+array.map(callback, thisArg);
+
+/*
+callback: função a ser executada em cada elemento
+thisArg(opcional): valor de 'this' dentro da função de callback
+*/
