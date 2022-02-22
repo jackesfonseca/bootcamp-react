c3674460718dda9a1274d1b8342b9e10d5810460
Jackes da Fonseca <53023400+jackesfonseca@users.noreply.github.com>
2022-02-22 14:56:46 -0300

    Create for_each.js

---
+++ map/for_each.js
@@ -0,0 +1,12 @@
+/*
+Faz esencialemnte a mesma coisa que  o map
+*/
+
+
+//usando map
+const array = [1, 3, 3, 4, 5];
+array.map((item) => item*2); //retorna: [2, 4, 6, 8, 10]
+
+//usando for each
+const array = [1, 2, 3, 4, 5];
+array.forEach((item) => item*2); //retorna undefined
