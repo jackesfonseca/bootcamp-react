f55051e7755960a25c853ef5803121e9d88bd2d4
Jackes Tiago Ferreira da Fonseca <jackes.fonseca@faciplac.edu>
2022-03-10 15:54:55 -0300

    updatng path names

---
+++ funções/funcoes_anonimas.js
@@ -0,0 +1,8 @@
+/* expressão de função ou função anônima */
+
+const soma = function (a, b) {
+	return a + b; 
+}
+
+soma(1, 2)
+soma(3, 4)
---
+++ funções/funcoes_autoinvocaveis.js
@@ -0,0 +1,16 @@
+/* podem ser anônimas ou não */
+
+(
+	function()
+	{
+		let name = "Jackes";
+		return name;
+	}
+)();
+
+const soma = (
+	function()
+	{
+		return a+b;
+	}
+)(1, 2);
